dbef814cabb2adca1b53a60fa342701d436f389f
gelacki <gelacki.chowa@gmail.com>
2026-09-09 12:59:42 -0300

    Resolve exercicio 6.

--- exercicios.js
+++ exercicios.js
@@ -33,27 +33,27 @@
 
 // 5. Receber numeros até digitar 0 (simulado com array)
 
-let numeroDigitado;
-let quantidade = 0;
-let repeticoes = 0;
+//let numeroDigitado;
+//let quantidade = 0;
+//let repeticoes = 0;
 
-do {
+//do {
     //simulação manuel de entrada
-    if (repeticoes === 0) {
-        numeroDigitado = 5;
-    } else if (repeticoes === 1) {
-        numeroDigitado = 10;
-    } else {numeroDigitado = 0;
-}
+    //if (repeticoes === 0) {
+        //numeroDigitado = 5;
+    //} else if (repeticoes === 1) {
+        //numeroDigitado = 10;
+    //} else {numeroDigitado = 0;
+//}
 
-if (numeroDigitado !== 0) {
-    quantidade++;
-}
+//if (numeroDigitado !== 0) {
+    //quantidade++;
+//}
 
-repeticoes++;
-} while (numeroDigitado !== 0);
+//repeticoes++;
+//} while (numeroDigitado !== 0);
 
-console.log('Total de números digitados: ' + quantidade)
+//console.log('Total de números digitados: ' + quantidade)
 
 // 6. Jogo do numero secreto
 
